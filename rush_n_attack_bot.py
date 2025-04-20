9f3e0273f581c686bce15de61a877d955df2e891
Vinícius Souza <viniso1903@gmail.com>
2025-04-20 19:31:13 -0300

    translate to english

--- rush_n_attack_bot.py
+++ rush_n_attack_bot.py
@@ -1,43 +1,42 @@
 import retro
 import numpy as np
-import time
 import cv2
 
-def encontrar_player(imagem_binaria, bloco_altura=120, bloco_largura=60):
-    altura, largura = imagem_binaria.shape
-    max_densidade = 0
-    posicao_max = (0, 0)
-    for y in range(0, altura, 10):
-        for x in range(0, largura, 10):
-            bloco = imagem_binaria[y:y+bloco_altura, x:x+bloco_largura]
-            densidade = np.sum(bloco == 60)
-            if densidade > max_densidade:
-                max_densidade = densidade
-                posicao_max = (x, y)
-    return posicao_max
+def find_player(binary_image, block_height=120, block_widght=60):
+    height, widght = binary_image.shape
+    max_density = 0
+    position = (0, 0)
+    for y in range(0, height, 10):
+        for x in range(0, widght, 10):
+            block = binary_image[y:y+block_height, x:x+block_widght]
+            density = np.sum(block == 60)
+            if density > max_density:
+                max_density = density
+                position = (x, y)
+    return position
 
-def encontrar_inimigo_1(imagem_binaria, bloco_altura=60, bloco_largura=60):
-    altura, largura = imagem_binaria.shape
-    distancia = 80
-    min_densidade_branco = 200
-    min_densidade_preto = 60
-    posicoes = []
-    for y in range(0, altura, bloco_altura):
-        for x in range(0, largura, bloco_largura):
-            bloco = imagem_binaria[y:y+bloco_altura, x:x+bloco_largura]
-            densidade = np.sum(bloco == 250)
-            if densidade > min_densidade_branco:
-                bloco = imagem_binaria[y+60:y+60+bloco_altura, x:x+bloco_largura]
-                densidade = np.sum(bloco == 0)
-                if densidade > min_densidade_preto:
-                    pos_nova = (x, y)
-                    ja_detectado = False
-                    for pos in posicoes:
-                        if np.linalg.norm(np.array(pos_nova) - np.array(pos)) < distancia:
-                            ja_detectado = True
-                    if not ja_detectado:
-                        posicoes.append(pos_nova)
-    return posicoes
+def find_enemy_1(binary_image, block_height=60, block_widght=60):
+    height, widght = binary_image.shape
+    distance = 80
+    min_density_white = 200
+    min_density_black = 60
+    positions = []
+    for y in range(0, height, block_height):
+        for x in range(0, widght, block_widght):
+            block = binary_image[y:y+block_height, x:x+block_widght]
+            density = np.sum(block == 250)
+            if density > min_density_white:
+                block = binary_image[y+60:y+60+block_height, x:x+block_widght]
+                density = np.sum(block == 0)
+                if density > min_density_black:
+                    new_pos = (x, y)
+                    detected = False
+                    for pos in positions:
+                        if np.linalg.norm(np.array(new_pos) - np.array(pos)) < distance:
+                            detected = True
+                    if not detected:
+                        positions.append(new_pos)
+    return positions
 
 def main():
     env = retro.make(game="RushnAttack-Nes")
@@ -47,22 +46,24 @@ def main():
             frame = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
             frame = cv2.resize(frame, (960, 900))
             frame = frame[200:frame.shape[0]-55,:]
-            intervalo = 10
-            frame = (frame // intervalo) * intervalo
-            pos = encontrar_player(frame)
-            posicoes = encontrar_inimigo_1(frame)
-            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+            gap = 10
+            frame = (frame // gap) * gap
+            pos = find_player(frame)
+            positions = find_enemy_1(frame)
             x, y = pos
+            ########## Debugging ###########
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
             cv2.rectangle(frame, (x, y), (x+60, y+120), (255, 0, 0), 2)
-            for (x, y) in posicoes:
+            for (x, y) in positions:
                 cv2.rectangle(frame, (x, y), (x + 60, y + 120), (0, 0, 255), 2)
+            ########## Debugging ###########
             cv2.imshow("Rush'n Attack - NES", frame)
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
             #time.sleep(0.02)
             if done:
                 obs = env.reset()
-            # Pressione ESC para sair
+            # ESC
             if cv2.waitKey(1) & 0xFF == 27:
                 break
     env.close()
