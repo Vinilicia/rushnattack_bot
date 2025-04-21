00283b0f259d7eb0bc087cf5df83a4d09c1923c6
Vinícius Souza <viniso1903@gmail.com>
2025-04-20 23:30:12 -0300

    potential field

--- rush_n_attack_bot.py
+++ rush_n_attack_bot.py
@@ -15,20 +15,25 @@ def find_player(binary_image, block_height=120, block_widght=60):
                 position = (x, y)
     return position
 
-def find_enemy_1(binary_image, block_height=60, block_widght=60):
+def find_enemy_1(binary_image, positions, block_height=60, block_widght=60):
     height, widght = binary_image.shape
-    distance = 80
-    min_density_white = 200
-    min_density_black = 60
-    positions = []
+    distance = 100
+    min_density_white = 250
+    min_density_black = 250
+    max_density_blue = 200
+    max_density_gray = 1000
     for y in range(0, height, block_height):
         for x in range(0, widght, block_widght):
             block = binary_image[y:y+block_height, x:x+block_widght]
             density = np.sum(block == 250)
-            if density > min_density_white:
+            density_blue = np.sum(block == 60)
+            density_gray = np.sum(block == 110)
+            if density > min_density_white and density_blue < max_density_blue and density_gray < max_density_gray:
                 block = binary_image[y+60:y+60+block_height, x:x+block_widght]
                 density = np.sum(block == 0)
-                if density > min_density_black:
+                density_blue = np.sum(block == 60)
+                density_gray = np.sum(block == 110)
+                if density > min_density_black and density_blue < max_density_blue and density_gray < max_density_gray:
                     new_pos = (x, y)
                     detected = False
                     for pos in positions:
@@ -36,30 +41,57 @@ def find_enemy_1(binary_image, block_height=60, block_widght=60):
                             detected = True
                     if not detected:
                         positions.append(new_pos)
-    return positions
+
+def closest_enemy(player, positions):
+    min = 50000
+    min_idx = 0
+    for idx, position in enumerate(positions):
+        distance = np.linalg.norm(np.array(player) - np.array(position))
+        if distance < min:
+            min_idx = idx
+            min = distance
+    return min_idx, min
 
 def main():
     env = retro.make(game="RushnAttack-Nes")
     obs = env.reset()
     while True:
             env.render()
+            ###### ['B', 'A', 'SELECT', 'START', 'UP', 'DOWN', 'LEFT', 'RIGHT'] #####
+            action = np.zeros(env.action_space.shape[0], dtype=np.uint8)
             frame = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
             frame = cv2.resize(frame, (960, 900))
             frame = frame[200:frame.shape[0]-55,:]
             gap = 10
             frame = (frame // gap) * gap
-            pos = find_player(frame)
-            positions = find_enemy_1(frame)
-            x, y = pos
+            player_position = find_player(frame)
+            enemy_positions = []
+            find_enemy_1(frame, enemy_positions)
+            x, y = player_position
             ########## Debugging ###########
             frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
             cv2.rectangle(frame, (x, y), (x+60, y+120), (255, 0, 0), 2)
-            for (x, y) in positions:
+            for (x, y) in enemy_positions:
                 cv2.rectangle(frame, (x, y), (x + 60, y + 120), (0, 0, 255), 2)
             ########## Debugging ###########
-            cv2.imshow("Rush'n Attack - NES", frame)
-            action = env.action_space.sample()
+            if enemy_positions:
+                enemy, dist = closest_enemy(player_position, enemy_positions)
+                if dist < 80:
+                    action[0] = 1 # B
+                else:
+                    if player_position[0] < enemy_positions[enemy][0]:
+                        #pass
+                        action[7] = 1 # Right
+                    else:
+                        #pass
+                        action[6] = 1 # Left
+            else:
+                action[7] = 1 # Right
             obs, reward, done, info = env.step(action)
+            #r = 110
+            #frame = cv2.inRange(frame, r, r)
+            cv2.imshow("Rush'n Attack - NES", frame)
+            #action = env.action_space.sample()
             #time.sleep(0.02)
             if done:
                 obs = env.reset()
